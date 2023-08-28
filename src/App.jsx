83b8077d7bbd75bdbf1f422f52be1c9086a35d9f
anyajegorova <anna.jerm@outlook.com>
2023-08-28 18:15:53 +0300

    navigation and new avatar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState, useRef } from 'react'
 import './App.css'
 import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
-
 import Avatar from './assets/avatar.png';
+import Avatar2 from './assets/avatar2.png';
+import Navbar from './components/Navbar';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,24 +14,40 @@ function App() {
     <LocomotiveScrollProvider options={options} containerRef={ref}>
       <main data-scroll-container ref={ref}>
         <section id='landing_page' data-scroll-section>
+          <Navbar />
           <div className='section-container'>
             <div className='main-info'>
-              <img src={Avatar} alt='avatar image' />
+              <img src={Avatar2} alt='avatar image' />
               <div className='description'>
                 <span>Hello and Welcome!<br />
                   My name is <span className='orange-text'>Anna Jegorova</span> and I’m a future <span className='orange-text'>Front-End Developer</span>.</span></div>
             </div>
+            <div className='secondary-info'>
+              <div className='secondary-description'>
+
+              </div>
+              <div className='secondary-description'>
+
+              </div>
+              <div className='secondary-description'>
+
+              </div>
+            </div>
           </div>
         </section>
+
         <section id='about_page' data-scroll-section>
           <h1>About Page</h1>
         </section>
+
         <section id='project_page' data-scroll-section>
           <h1>Project Page</h1>
         </section>
+
         <section id='contact_page' data-scroll-section>
           <h1>Contact Page</h1>
         </section>
+
       </main>
     </LocomotiveScrollProvider>
   )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <div className='navbar-container'>
+            <nav>
+                <a>About</a>
+                <a>Projects</a>
+                <a>Contact</a>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
