528504607bd1c4f59d598f2cae087835c0dd1589
anyajegorova <anna.jerm@outlook.com>
2023-08-29 15:48:53 +0300

    about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState, useRef } from 'react'
 import './App.css'
 import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
+
 import Avatar from './assets/avatar.png';
 import Avatar2 from './assets/avatar2.png';
+import Coding from './assets/grey_coding.png';
+import Design from './assets/grey_design.png';
+
 import Navbar from './components/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const ref = useRef(null);
-  const options = { smooth: true, multiplier: 4 }
+  const options = { smooth: true, multiplier: 3 }
 
   return (
     <LocomotiveScrollProvider options={options} containerRef={ref}>
@@ -24,20 +28,42 @@ function App() {
             </div>
             <div className='secondary-info'>
               <div className='secondary-description'>
+                <img src={Design} alt='design icon' />
+                <h5>UX/UI Design</h5>
 
+                <div className='description-container'>
+                  I utilize the best design practices for shaping
+                  holistic  and modern interfaces
+                </div>
               </div>
               <div className='secondary-description'>
-
+                <img src={Coding} alt='coding icon' />
+                <h5>Software Development</h5>
+                <div className='description-container'>
+                  ..and turn them into code using latest
+                  technologies
+                </div>
               </div>
-              <div className='secondary-description'>
 
-              </div>
             </div>
           </div>
         </section>
 
         <section id='about_page' data-scroll-section>
-          <h1>About Page</h1>
+          <div className='about_page-container'>
+            <div className='heading'>Designing + Coding</div>
+            <div className='technologies-list'>
+              <ul>
+                <li>Javascript</li>
+                <li>ReactJS</li>
+                <li>NodeJS</li>
+                <li>PostgreSQL</li>
+                <li>MongoDB</li>
+                <li>Figma</li>
+              </ul>
+
+            </div>
+          </div>
         </section>
 
         <section id='project_page' data-scroll-section>
