c13f8c62e5d920a5c6478f3fbebe50f9137a0ea0
anyajegorova <anna.jerm@outlook.com>
2023-09-03 02:02:10 +0300

    options added to projects view

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -9,46 +9,83 @@ import CourseApp from '../project_images/courseapp_marvel.png';
 
 const Projects = () => {
     const [currentImage, setCurrentImage] = useState(Tastybites);
-    // const images = [{ name: 'Project 1', src: Cat1 }, { name: 'Project 2', src: Cat2 }, { id: 'Project 3', src: Cat3 }, { id: 'Project 4', src: Cat4 }]
+    const [option, setOption] = useState('design');
 
-    // const handleChange = (e) => {
-    //     console.log(currentImage);
-    //     console.log(e.target)
-    //     for (let i = 1; i <= images.length - 1; i++) {
-    //         e.target.value == images[i].name ? setCurrentImage(images[i].src) : setCurrentImage('Error');
-    //         console.log(images[i].name)
-    //     }
-    // }
+    useEffect(() => {
+        toggleOption();
+    }, []);
 
+    const toggleOption = () => {
+        if (option == 'design') {
+            return (
+                <div>
+                    <div className='project-name' onMouseOver={() => setCurrentImage(CourseApp)}>
+                        <h5>UX App</h5>
+                        <h6>Team Project, Axure</h6>
+                    </div>
 
-
-
-
-    return (
-        <>
-            <div className='main-container'>
-                <div className='projects-list' data-scroll data-scroll-speed="3">
                     <div className='project-name' onMouseOver={() => setCurrentImage(Tastybites)}>
                         <h5>Tastybites</h5>
                         <h6>UI Design, Figma</h6>
                     </div>
+
                     <div className='project-name' onMouseOver={() => setCurrentImage(Room)}>
                         <h5>3D Room</h5>
                         <h6>3D Design, Blender</h6>
                     </div>
+
                     <div className='project-name' onMouseOver={() => setCurrentImage(Clock)}>
                         <h5>3D Clock</h5>
                         <h6>3D Design, Blender</h6>
                     </div>
+                </div>
+            )
+        } else if (option == 'code') {
+            return (
+                <div>
                     <div className='project-name' onMouseOver={() => setCurrentImage(CourseApp)}>
-                        <h5>UX App</h5>
-                        <h6>Team Project, Marvel</h6>
+                        <h5>Project 1</h5>
+                        <h6>Webpage, ReactJS</h6>
+                    </div>
+
+                    <div className='project-name' onMouseOver={() => setCurrentImage(Tastybites)}>
+                        <h5>Tastybites</h5>
+                        <h6>HTML, CSS, ReactJS</h6>
+                    </div>
+
+                    <div className='project-name' onMouseOver={() => setCurrentImage(Room)}>
+                        <h5>Project 3</h5>
+                        <h6>Mobile App, React Native</h6>
+                    </div>
+
+                    <div className='project-name' onMouseOver={() => setCurrentImage(Clock)}>
+                        <h5>Portfolio</h5>
+                        <h6>HTML, CSS, ReactJS</h6>
                     </div>
                 </div>
-                <div className='projects-image'>
-                    <img src={currentImage} alt='image of the project' />
+            )
+        }
+    }
+
+    return (
+        <>
+            <div>
+                <div className='toggle-container' data-scroll data-scroll-speed="3">
+                    <button className='toggle-button' id={option === 'design' ? 'selected' : 'notSelected'} onClick={() => setOption('design')}>Design</button>
+                    <button className='toggle-button' id={option === 'code' ? 'selected' : 'notSelected'} onClick={() => setOption('code')}>Code</button>
+                </div>
+                <div className='main-container'>
+
+                    <div className='projects-list' data-scroll data-scroll-speed="3">
+                        {toggleOption()}
+                    </div>
+                    <div className='projects-image'>
+                        <img src={currentImage} alt='image of the project' />
+                    </div>
                 </div>
             </div>
+
+
         </>
     )
 }
